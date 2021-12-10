9ea9357a61b4ca57fd4d28f8f702908be593f49d
Adrien Belminksy <belminksy@gmail.com>
2021-12-10 16:00:38 +0100

    feat: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,49 @@
  */
 
 import { NestFactory } from '@nestjs/core'
+import {
+    ArgumentsHost,
+    Catch,
+    ExceptionFilter,
+    HttpException,
+    HttpStatus
+} from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { Response } from 'express'
 import AppModule from './app.module'
+
+@Catch()
+class AllExceptionsFilter implements ExceptionFilter {
+
+    catch(exception: unknown, host: ArgumentsHost) {
+
+        const ctx = host.switchToHttp()
+        const response = ctx.getResponse<Response>()
+
+        let statusCode = HttpStatus.INTERNAL_SERVER_ERROR
+        let message = 'Internal Server Error'
+
+        if (exception instanceof HttpException) {
+            statusCode = exception.getStatus()
+            message = exception.message
+        }
+
+        response
+            .status(statusCode)
+            .json({
+                status: 'failure',
+                statusCode,
+                message
+            })
+    }
+
+}
 
 const bootstrap = async () => {
 
     const app = await NestFactory.create(AppModule)
+
+    app.useGlobalFilters(new AllExceptionsFilter())
 
     const configService = app.get<ConfigService>(ConfigService)
 
